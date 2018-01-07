d6eab92279ede3439863852a1e31965928f53ef8
Dmitriy Ievlev <IevlevDmitriy@gmail.com>
2018-01-08 01:59:07 +0200

    code refactoring and deduplication

--- cypress/integration/sitegen_registered_checkout_spec.js
+++ cypress/integration/sitegen_registered_checkout_spec.js
@@ -1,7 +1,5 @@
-// login
-
 describe('Registered user checkout', function(){
-  it('has to open PDP, add product to a cart and pass checkout with a Credit Card', function(){
+  beforeEach(() => {
     // *** PDP ***
     cy.visit('https://sitegenesis.demandware.net/640188017003.html');
     cy.get('#add-to-cart').click();
@@ -39,7 +37,9 @@ describe('Registered user checkout', function(){
     cy.get('[name$=_states_state]').select('CA');
     cy.get('[name$=_phone]').clear().type('415-901-7000');
     cy.get('[name$=_emailAddress]').clear().type('user@example.com');
-    
+  });
+
+  it('has to open PDP, add product to a cart and pass checkout with a Credit Card', function(){
     // CC-fields
     cy.get('[name$=_creditCard_owner]').type('Test Testoff');
     cy.get('[name$=_creditCard_type]').select('Visa');
@@ -57,45 +57,7 @@ describe('Registered user checkout', function(){
     cy.url().should('include', '/orderconfirmation'); 
   });
 
-  it('has to open PDP, add product to a cart and pass checkout with PayPal', function(){
-    // *** PDP ***
-    cy.visit('https://sitegenesis.demandware.net/640188017003.html');
-    cy.get('#add-to-cart').click();
-    cy.get('.mini-cart-link-cart').click();
-    
-    // *** CART ***
-    cy.get('form#checkout-form button[name$=_checkoutCart]:first').click();
-    
-    
-    // *** CHECKOUT STEP 1 ***
-    cy.get('[name*=_login_username]').clear().type('ievlevdmitriy@gmail.com');
-    cy.get('[name*=_login_password]').clear().type('ievlevdmitriy@gmail.com');
-    cy.get('button[type=submit][name$=_login]').click();  
-    
-    // *** CHECKOUT STEP 2 - Shipping ***
-    // Shipping Address
-    cy.get('[name$=_firstName]').clear().type('Test');
-    cy.get('[name$=_lastName]').clear().type('Testoff');
-    cy.get('[name$=_address1]').clear().type('1 main str');
-    cy.get('[name$=_city]').clear().type('San Jose');
-    cy.get('[name$=_postal]').clear().type('95136');
-    cy.get('[name$=_states_state]').select('CA');
-    cy.get('[name$=_phone]').clear().type('555-555-5555');
-    
-    cy.get('form[id$=_shippingAddress] button[type=submit]').should('not.be.disabled').click();
-    
-    // *** CHECKOUT STEP 3 ***
-    // Billing Address
-    cy.get('[name$=_firstName]').clear().type('John');
-    cy.get('[name$=_lastName]').clear().type('Doe');
-    cy.get('[name$=_address1]').clear().type('1 Market St');
-    cy.get('[name$=_address2]').clear().type('Suite 300');
-    cy.get('[name$=_city]').clear().type('San Francisco');
-    cy.get('[name$=_postal]').clear().type('94105');
-    cy.get('[name$=_states_state]').select('CA');
-    cy.get('[name$=_phone]').clear().type('415-901-7000');
-    cy.get('[name$=_emailAddress]').clear().type('user@example.com');
-    
+  it('has to open PDP, add product to a cart and pass checkout with PayPal', function(){    
     // select PayPal
     cy.get('#is-PayPal[type=radio]').click(); 
     
@@ -108,45 +70,7 @@ describe('Registered user checkout', function(){
     cy.url().should('include', '/orderconfirmation'); 
   });
 
-  it('has to open PDP, add product to a cart and pass checkout with PayPal', function(){
-    // *** PDP ***
-    cy.visit('https://sitegenesis.demandware.net/640188017003.html');
-    cy.get('#add-to-cart').click();
-    cy.get('.mini-cart-link-cart').click();
-    
-    // *** CART ***
-    cy.get('form#checkout-form button[name$=_checkoutCart]:first').click();
-    
-    
-    // *** CHECKOUT STEP 1 ***
-    cy.get('[name*=_login_username]').clear().type('ievlevdmitriy@gmail.com');
-    cy.get('[name*=_login_password]').clear().type('ievlevdmitriy@gmail.com');
-    cy.get('button[type=submit][name$=_login]').click();  
-    
-    // *** CHECKOUT STEP 2 - Shipping ***
-    // Shipping Address
-    cy.get('[name$=_firstName]').clear().type('Test');
-    cy.get('[name$=_lastName]').clear().type('Testoff');
-    cy.get('[name$=_address1]').clear().type('1 main str');
-    cy.get('[name$=_city]').clear().type('San Jose');
-    cy.get('[name$=_postal]').clear().type('95136');
-    cy.get('[name$=_states_state]').select('CA');
-    cy.get('[name$=_phone]').clear().type('555-555-5555');
-    
-    cy.get('form[id$=_shippingAddress] button[type=submit]').should('not.be.disabled').click();
-    
-    // *** CHECKOUT STEP 3 ***
-    // Billing Address
-    cy.get('[name$=_firstName]').clear().type('John');
-    cy.get('[name$=_lastName]').clear().type('Doe');
-    cy.get('[name$=_address1]').clear().type('1 Market St');
-    cy.get('[name$=_address2]').clear().type('Suite 300');
-    cy.get('[name$=_city]').clear().type('San Francisco');
-    cy.get('[name$=_postal]').clear().type('94105');
-    cy.get('[name$=_states_state]').select('CA');
-    cy.get('[name$=_phone]').clear().type('415-901-7000');
-    cy.get('[name$=_emailAddress]').clear().type('user@example.com');
-    
+  it('has to open PDP, add product to a cart and pass checkout with PayPal', function(){    
     // select BML
     cy.get('#is-BML[type=radio]').click();    
     cy.get('[name$=_billing_paymentMethods_bml_year]').select('1970');
